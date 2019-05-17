22b3b90824f303f088a63c2519b1e2c803f1ee64
sosolean <35632385+sosolean@users.noreply.github.com>
2019-05-17 15:17:46 +0100

    Update mech_actions.sh
    updated variables for ENU

--- scripts/mech_actions.sh
+++ scripts/mech_actions.sh
@@ -1,16 +1,16 @@
 #!/bin/sh
 #
-# EOS Mechanics actions script.
+# ENO Mechanics actions script.
 #
 
-API_URL="http://127.0.0.1:8888"
+API_URL="https://api.enumivo.org"
 WALLET_URL="http://127.0.0.1:8900/"
-WALLET_NAME="eosmechanics"
+WALLET_NAME="enomechanics"
 WALLET_PASS="password to eosmechanics wallet with benchmark permission key"
-PATH="$PATH:/usr/local/bin:/usr/local/eosio/bin/"
+PATH="$PATH:/usr/local/bin:/usr/local/enumivo/bin/"
 
 date
 cleos -u "$API_URL" --wallet-url "$WALLET_URL" wallet unlock -n "$WALLET_NAME" --password "$WALLET_PASS"
-cleos -u "$API_URL" --wallet-url "$WALLET_URL" push action eosmechanics cpu '' -p eosmechanics@benchmark -f
-#cleos -u "$API_URL" --wallet-url "$WALLET_URL" push action eosmechanics ram '' -p eosmechanics@benchmark -f
+cleos -u "$API_URL" --wallet-url "$WALLET_URL" push action enomechanics cpu '' -p enomechanics@benchmark -f
+#cleos -u "$API_URL" --wallet-url "$WALLET_URL" push action enomechanics ram '' -p enomechanics@benchmark -f
 cleos -u "$API_URL" --wallet-url "$WALLET_URL" wallet stop
